9a541e85198107bfefabc98cfc8a55fb1bf374f2
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:39:12 +0600

    Create Mission.py

---
+++ Mission.py
@@ -0,0 +1,9 @@
+def mission():
+    while mission>0:
+        if life>0 and not area and not quest and not cross:
+            print("Welcome new area")
+            score+=5;
+        if life==0 
+        print("Dead");
+        else:
+            print("Pass mission successfully!");
