3fa21d8cb1d0bcc519c8784569f10ca4cea314e4
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:27:30 +0600

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,8 @@
+questions = 
+[ 
+    "What is the capital of France?",
+    "Who wrote the play 'Romeo and Juliet'?",
+    "What is the largest planet in our solar system?", 
+    "What is the chemical symbol for water?", 
+    "Who painted the Mona Lisa?" 
+]
