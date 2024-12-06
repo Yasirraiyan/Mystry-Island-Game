265dd765cde6745dc2ed13cfd839e3525f6c9661
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:29:31 +0600

    Create Prerequisite.py

---
+++ Prerequisite.py
@@ -0,0 +1,70 @@
+questions = 
+[ 
+    "What is the capital of France?",
+    "Who wrote the play 'Romeo and Juliet'?",
+    "What is the largest planet in our solar system?", 
+    "What is the chemical symbol for water?", 
+    "Who painted the Mona Lisa?" 
+]
+answers 
+=
+[
+    "Paris", "William Shakespeare", "Jupiter", "H2O", "Leonardo da Vinci"
+]
+map_array 
+=
+[
+    "Jungle", "Cave", "River", "Old Home", "Jungle", "River", "Cave", "River", "Jungle"
+    ]
+characters = 
+[ 
+    { "name": "Hero", "powers":
+        ["Strength", "Agility", "Flight"], 
+        "things": ["Sword", "Shield", "Potion"]
+        }, 
+        { 
+            "name": "Wizard", "powers": 
+                [
+                    "Magic", "Teleportation", "Invisibility"
+                    ],
+                    "things": 
+                        ["Wand", "Spell Book", "Crystal Ball"] 
+            
+        },
+        { 
+            "name": "Rogue", "powers":
+                ["Stealth", "Speed", "Lockpicking"],
+                "things": ["Dagger", "Lockpick", "Cloak"] 
+            
+        },
+        { 
+            "name": "Healer", "powers":
+                ["Healing", "Revival", "Protection"],
+                "things": 
+                    [
+                        "Healing Staff", "Herbs", "Amulet"
+                        ] 
+        } 
+        ]
+        mission=5;
+        quest=False;
+        area=False;
+        solve=False;
+        chance=5;
+        life=5;
+        score=0;
+        allsuccess=False;
+        cross=False;
+        power=False;
+        thing=False;
+        controlmechanism=False;
+        clue=0;
+        info=0;
+        explain=False;
+        competition=False;
+        challange=False;
+        lock=False;
+        unlock=False;
+        skill=0;
+        items = ["Pocket", "Diagram", "Magnifying Glass", "Torch"]    
+        equipment = ["Pocket", "Diagram", "Magnifying Glass", "Torch"]
