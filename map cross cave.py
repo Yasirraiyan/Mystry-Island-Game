e7e010811c72d79e9cb112e900dec7aa92c4ead9
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:37:02 +0600

    Create map cross cave.py

---
+++ map cross cave.py
@@ -0,0 +1,6 @@
+def mapcrosscave():
+    if map in map_array:
+        if map==cave and not cross:
+            print("Cross the cave successfully.")
+            else:
+                print("Not success!")
