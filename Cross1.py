f5c2c557fe36f1d7cc0639f411c1dc1cc838bd46
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:31:31 +0600

    Create Cross1.py

---
+++ Cross1.py
@@ -0,0 +1,7 @@
+def cross1(mission,quest,area,solve)   :
+            if not mission and not quest and not area and not solve:
+                print("Cross mission1 successfully.Go next step.")
+                return (mission,quest,area,solve)
+                 if life==0 or chance==0:
+                else:
+                    print("Not success.Fail")
