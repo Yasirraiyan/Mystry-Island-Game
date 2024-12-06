c49fd346b55921d36eb1b7659bee0ad7e8c5fc31
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:28:50 +0600

    Create characters.py

---
+++ characters.py
@@ -0,0 +1,30 @@
+characters = 
+[ 
+    { "name": "Hero", "powers":
+        ["Strength", "Agility", "Flight"], 
+        "things": ["Sword", "Shield", "Potion"]
+        }, 
+        { 
+            "name": "Wizard", "powers": 
+                [
+                    "Magic", "Teleportation", "Invisibility"
+                    ],
+                    "things": 
+                        ["Wand", "Spell Book", "Crystal Ball"] 
+            
+        },
+        { 
+            "name": "Rogue", "powers":
+                ["Stealth", "Speed", "Lockpicking"],
+                "things": ["Dagger", "Lockpick", "Cloak"] 
+            
+        },
+        { 
+            "name": "Healer", "powers":
+                ["Healing", "Revival", "Protection"],
+                "things": 
+                    [
+                        "Healing Staff", "Herbs", "Amulet"
+                        ] 
+        } 
+        ]
