3ee6d3542c26daab7223439b31b7304e2a712c8b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:32:08 +0600

    Create Control Mechanism2.py

---
+++ Control Mechanism2.py
@@ -0,0 +1,14 @@
+  def controlmechanism2()  :
+          while :
+              life>0 and chance>0:
+                  if not solve and
+                      characters in characters and question in questions and answer in answer and equipment in equipment :
+                          solve=True;
+                          chance-=2;
+                          life-=2;
+                          score2=score+10;
+                          quest=True
+                          area=True
+                          solve=True
+                          skill2=skill+20;
+                          mission--;
