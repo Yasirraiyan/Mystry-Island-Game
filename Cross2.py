7a39fec7911131de7b2d9f93ca5242cf12fc8386
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:32:37 +0600

    Create Cross2.py

---
+++ Cross2.py
@@ -0,0 +1,24 @@
+                  
+        def cross2(mission,quest,area,solve)   :
+            if not mission and not quest and not area and not solve:
+                print("Cross mission2 successfully.Go next step.")
+                return (mission,quest,area,solve)
+                 if life==0 or chance==0:
+                else:
+                    print("Not success.Fail")
+                    def controlmechanism1()  :
+          while :
+              life>0 and chance>0:
+                  if not solve and
+                      characters in characters and question in questions and answer in answer and equipment in equipment :
+                          solve=True;
+                          chance-=3;
+                          life-=3;
+                          score3=score+15;
+                          cross=True
+                          quest=True
+                          area=True
+                          solve=True
+                          skill3=skill+30;
+                          mission--
+                          
