3e802a3af82a6e7459d1b00816e2a31b73a26eff
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:36:39 +0600

    Create Map cross forest.py

---
+++ Map cross forest.py
@@ -0,0 +1,6 @@
+def mapcrossforest():
+    if map in map_array:
+        if map==forest and not cross:
+            print("Cross the forest successfully.")
+            else:
+                print("Not success!")
