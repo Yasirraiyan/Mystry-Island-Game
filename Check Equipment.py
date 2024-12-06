6aabd5bbd2810808e0090c832c58baf68ed150fb
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:40:39 +0600

    Create Check Equipment.py

---
+++ Check Equipment.py
@@ -0,0 +1,10 @@
+def checkequipment():
+    if equipment in equipment:
+    print("Valid equipment!")
+    else:
+        print("Invalid equipment!")
+    def solvepuzzle():
+        if not controlmechanism:
+            print("Puzzle solve successfully!")
+        else:
+            print("Fail!")
