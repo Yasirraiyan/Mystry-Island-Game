094a490a3a46dc2348c786a03bc3a264e0fc057a
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:36:04 +0600

    Create Map Cross Area.py

---
+++ Map Cross Area.py
@@ -0,0 +1,6 @@
+def mapcrossarea():
+    if map in map_array:
+        if map==area and not cross:
+            print("Cross the area successfully.")
+            else:
+                print("Not success!")
