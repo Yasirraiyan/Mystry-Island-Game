3f23a75858b98bcc97e2eb1bb27861b0c62c5231
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:37:47 +0600

    Create map cross river.py

---
+++ map cross river.py
@@ -0,0 +1,6 @@
+def mapcrossriver():
+    if map in map_array:
+        if map==river and not cross:
+            print("Cross the river successfully.")
+            else:
+                print("Not success!")
