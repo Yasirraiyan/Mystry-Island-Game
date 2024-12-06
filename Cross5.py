c31059715c3c7db3ee1038e3dd41c0a7302281b1
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:35:28 +0600

    Create Cross5.py

---
+++ Cross5.py
@@ -0,0 +1,20 @@
+ def cross5(mission,quest,area,solve)   :
+            if not mission and not quest and not area and not solve:
+                print("Cross mission3 successfully.Go next step.")
+                return (mission,quest,area,solve)
+                 if life==0 or chance==0:
+                    print("Not success.Fail")
+                    def controlmechanism1()  :
+          while :
+              life>0 and chance>0:
+                  if not solve and
+                      characters in characters and question in questions and answer in answer and equipment in equipment :
+                          solve=True;
+                          chance--;
+                          life--;
+                          score5=score+25;
+                          quest=True
+                          area=True
+                          solve=True
+                          skill6=skill+60;
+                          mission--
