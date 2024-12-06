26932c3b4ee1547a413989b17aee2d0605e8859d
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 22:38:27 +0600

    Create Map cross old house.py

---
+++ Map cross old house.py
@@ -0,0 +1,6 @@
+def mapcrossoldhouse():
+    if map in map_array:
+        if map==cave and not cross:
+            print("Cross the old house successfully.")
+            else:
+                print("Not success!")
